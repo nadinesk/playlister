0fc02a18b7b469ec6549c787b16a5a456b7ffe20
nadinesk <nadine.khattak@gmail.com>
2017-07-09 00:44:34 -0400

    fixed functionality after signing in with omniauth

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,10 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :happiness, :free_time)
   end
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   def account_update_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :happiness, :free_time)
   end
